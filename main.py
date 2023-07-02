6ecf827ef50f51507620c0ea8f94e25c922f1bd4
rushiruk <rushirukanakaratne@hotmail.com>
2023-07-02 13:04:23 +0100

    Added tenor and chart

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ with col1:
     st.text_input("Stamp duty", value=stamp_duty, disabled=True)
     loan = buy_price - deposit
     st.text_input("Loan", value=loan, disabled=True)
+    tenor = st.number_input(
+        "Investment timeframe (years)", value=30, step=1, min_value=10, max_value=100, format="%d"
+    )
 with col2:
     tax_rate = st.number_input("Tax rate (%)", value=35, min_value=0, step=1, format="%d")
     interest_rate = st.number_input("Interest rate (%)", value=8.0, min_value=0.0, step=0.5, format="%f")
@@ -31,19 +34,20 @@ with col3:
     st.text_input("Net mortgage payment after tax", value=net_payment, disabled=True)
 
 
+house_returns = loan_burn_down_df(buy_price, loan, payment, interest_rate, housing_rate, tenor)
+df_height = height = int(35.19 * (len(house_returns) + 1))
 col1, col2 = st.columns([0.6, 0.4], gap="large")
 with col1:
-    st.subheader("House and loan over 30 years")
-    st.dataframe(
-        loan_burn_down_df(buy_price, loan, payment, interest_rate, housing_rate),
-        use_container_width=True,
-        height=1100,
-    )
+    st.subheader(f"House and loan over {tenor} years")
+    st.dataframe(house_returns, use_container_width=True, height=df_height)
 
+stock_returns = stock_df(deposit + stamp_duty, stock_Rate, net_payment, tenor)
 with col2:
-    st.subheader("Stock market over 30 years")
-    st.dataframe(
-        stock_df(deposit + stamp_duty, stock_Rate, net_payment),
-        use_container_width=True,
-        height=1100,
-    )
+    st.subheader(f"Stock market over {tenor} years")
+    st.dataframe(stock_returns, use_container_width=True, height=df_height)
+
+_, col2, _ = st.columns([0.25, 0.5, 0.25], gap="large")
+with col2:
+    df = house_returns[["End of year equity"]].join(stock_returns[["Ending balance"]])
+    df.rename(columns={"End of year equity": "House", "Ending balance": "Shares"}, inplace=True)
+    st.line_chart(df, use_container_width=True, height=800)
